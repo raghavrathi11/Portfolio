f2c814aed2aa6ef23d21dbe8217cd9fb699cc630
raghavrathi11 <97093994+raghavrathi11@users.noreply.github.com>
2022-01-04 15:43:00 +0530

    Add files via upload

---
+++ Covid19 Portfolio Project.sql
@@ -0,0 +1,114 @@
+
+Select *
+From [portfolio project]..coviddeaths
+Where continent is not null
+Order by 3, 4
+
+--Select *
+--From [portfolio project]..covidvaccinations
+--Order by 3, 4
+
+-- Select Data that we are going to be using
+
+Select location, date, total_cases, new_cases, total_deaths, population
+From [portfolio project]..coviddeaths
+Where continent is not null
+Order by 1, 2
+
+
+-- Looking at the Total Cases vs Total deaths
+-- Shows likelihood of dying if you contract COVID-19 in India
+
+Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From [portfolio project]..coviddeaths
+Where location = 'India' and continent is not null
+Order by 1, 2
+
+
+-- Looking at Total Cases vs Population
+-- Shows what percentage of the Indian population has contracted COVID-19
+
+Select location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
+From [portfolio project]..coviddeaths
+Where location = 'India'
+Order by 1, 2
+
+-- Looking at countries with Highest Infection Rate compared to Population
+
+Select location, population, max(total_cases) as PeakInfectionCount, max((total_cases/population))*100 as PercentPopulationInfected
+From [portfolio project]..coviddeaths
+Where continent is not null
+Group by location, population
+Order by PercentPopulationInfected desc
+
+-- Looking at countries with Highest Death Count
+
+Select location, max(cast (total_deaths as int)) as TotalDeathCount
+From [portfolio project]..coviddeaths
+Where continent is not null
+Group by location
+Order by TotalDeathCount desc
+
+-- Looking at continents with Highest Death Count
+
+Select continent, max(cast (total_deaths as int)) as TotalDeathCount
+From [portfolio project]..coviddeaths
+Where continent is not null
+Group by continent
+Order by TotalDeathCount desc
+
+-- Global Numbers
+
+Select sum(new_cases) as Total_Cases, sum(cast(new_deaths as int)) as Total_Deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
+From [portfolio project]..coviddeaths
+where continent is not null
+-- Group by date
+order by 1,2
+
+--Looking at Total Population vs Vaccinations
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(convert(bigint,vac.new_vaccinations)) OVER (Partition by dea.location ORDER by dea.location, dea.date) as RollingPeopleVaccinated
+From [portfolio project]..coviddeaths dea
+Join [portfolio project]..covidvaccinations vac
+On dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+order by 2,3
+
+-- Use CTE to find Percentage of Population Vaccinated
+
+With Pop_Vs_Vac (Continent, Location, Date, Population, new_vaccinations, RollingPeopleVaccinated) 
+as 
+(Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(convert(bigint,vac.new_vaccinations)) OVER (Partition by dea.location ORDER by dea.location, dea.date) as RollingPeopleVaccinated
+From [portfolio project]..coviddeaths dea
+Join [portfolio project]..covidvaccinations vac
+On dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null)
+Select*, (RollingPeopleVaccinated/Population)*100
+from Pop_Vs_Vac
+
+
+-- Creating view to store data for later visualizations
+
+Create View TotalPopulationvsVaccination as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(convert(bigint,vac.new_vaccinations)) OVER (Partition by dea.location ORDER by dea.location, dea.date) as RollingPeopleVaccinated
+From [portfolio project]..coviddeaths dea
+Join [portfolio project]..covidvaccinations vac
+On dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+
+Create View CountrieswithHighestDeathCount as
+Select location, max(cast (total_deaths as int)) as TotalDeathCount
+From [portfolio project]..coviddeaths
+Where continent is not null
+Group by location
+
+-- Using View to to find Percentage of Population Vaccinated
+
+Select *, (RollingPeopleVaccinated/population)*100
+from TotalPopulationvsVaccination
